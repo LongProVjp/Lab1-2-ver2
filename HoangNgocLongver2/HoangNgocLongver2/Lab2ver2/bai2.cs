c718632513b12b7d8a3bee0972786764bc27af26
LongProVjp <104289088+LongProVjp@users.noreply.github.com>
2022-11-05 15:34:02 +0700

    Add files via upload

---
+++ HoangNgocLongver2/HoangNgocLongver2/Lab2ver2/bai2.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Text;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Lab2ver2
+{
+    internal class bai2
+    {
+
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.InputEncoding = Encoding.Unicode;
+            Tracuu();
+        }
+        //ham tu dien
+        static void Tracuu()
+        {
+            string[] FileContents = File.ReadAllLines("vnedict.txt");
+            Dictionary<string, string> dict = new Dictionary<string, string>();
+            foreach (string line in FileContents)
+            {
+                var keyvalue = Regex.Match(line, @"(.*):(.*)");
+                dict.Add(keyvalue.Groups[1].Value.Trim(), keyvalue.Groups[2].Value.Trim());
+            }
+
+            string keyinput = "";
+            string keyitem = dict[keyinput];
+            while (true)
+            {
+                Console.WriteLine("Hãy nhập từ cần tra: ");
+                keyinput = Console.ReadLine();
+                if (dict.ContainsKey(keyinput))
+                {
+                    Console.WriteLine(dict[keyinput]);
+                }
+                else
+                {
+                    Console.WriteLine("Không có từ này trong từ điển");
+                }
+            }
+        }
+    }
+}
